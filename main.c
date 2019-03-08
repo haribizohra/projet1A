c2af429431b67d80494a1ee6facb7efc4a915786
molka-mrad <molka.mrad@esprit.tn>
2019-03-08 01:22:18 +0100

    modification des modules

--- main.c
+++ main.c
@@ -5,19 +5,10 @@ int main()
 {
 
 	SDL_Event event;
-	SDL_Surface *screen =NULL;
-	SDL_Surface *bg[12];
-	SDL_Surface *buttons[4];
-	SDL_Surface *settings[4];
-	SDL_Surface *levels[7];
-	SDL_Surface *players[3];
-	SDL_Surface *logo =NULL;
-	SDL_Surface *Breturn;
-	SDL_Surface *black;
+	SDL_Surface *screen =NULL,*bg[12],*buttons[4],*settings[4],*levels[7],*players[3],*logo =NULL,*Breturn,*black;
 	SDL_Rect posButtons,posLevels,posLogo,posSettings,posPlayers,posBReturn,posBlack1,posBlack2;
-	int done=1,time=0,menu=1,level=1,multij=0,game=0,player=0;
+	int done=1,time=0,menu=1,level=1,selection=1,key=0,multij=0,game=0,player=0;
 	int nbg=0,nbut=0,nset=0,nlevel=0,nplayer=0;
-	int selection=1,key;
 	Mix_Music *music;
 	Mix_Chunk *effect;
 
@@ -27,10 +18,10 @@ int main()
 
 while(done==1)
 {
-
 	time=SDL_GetTicks();
 	
 	input(&done,&key);
+
 	update(key,&done,&menu,&selection,level,&multij,&game,&nbg,&nbut,&nset,&nlevel,&nplayer,effect,time,&player);
 
 	afficherMenu(screen,bg,buttons,settings,levels,players,logo,Breturn,black,posButtons,posLevels,posLogo,posSettings, posPlayers,posBReturn,posBlack1,posBlack2,menu,level,nbg,nbut,nset,nlevel,nplayer);
@@ -57,6 +48,7 @@ for(int i=0;i<3;i++)
 SDL_FreeSurface(Breturn);
 SDL_FreeSurface(black);
 Mix_FreeMusic(music);
+Mix_FreeChunk(effect);
 
 SDL_Quit;
 }
